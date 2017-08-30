458f27c9689bc580a63f90218836629065f8146e
antalovc <antal.ovchinnikov@gmail.com>
2017-08-30 12:13:56 +0300

    5.14 Проведено разбиение кода на модули

---
+++ js/picture.js
@@ -0,0 +1,23 @@
+'use strict';
+
+window.picture = (function () {
+
+  var setElementPictureData = function (pictureData, pictureElement, imageSelector, likesSelector, commentSelector) {
+    pictureElement.querySelector(imageSelector).src = pictureData.url;
+    pictureElement.querySelector(likesSelector).textContent = pictureData.likes;
+    pictureElement.querySelector(commentSelector).textContent = pictureData.comments.length;
+  };
+
+  var createPictureFromTemplate = function (pictureData, pictureTemplate, imageSelector) {
+    var pictureElement = pictureTemplate.cloneNode(true);
+    setElementPictureData(pictureData, pictureElement, imageSelector, '.picture-likes', '.picture-comments');
+    return pictureElement;
+  };
+
+
+  return {
+    setElementPictureData: setElementPictureData,
+    createPictureFromTemplate: createPictureFromTemplate
+  };
+
+})();
---
+++ js/preview.js
@@ -0,0 +1,42 @@
+'use strict';
+
+window.preview = (function () {
+
+  var galleryOverlay = document.querySelector('.gallery-overlay');
+  var galleryOverlayClose = galleryOverlay.querySelector('.gallery-overlay-close');
+
+  var onGalleryOverlayEscPress = function (evt) {
+    window.util.callIfEscEvent(evt, hideGalleryOverlay);
+  };
+
+  var onGalleryOverlayCloseEnterPress = function (evt) {
+    window.util.callIfEnterEvent(evt, hideGalleryOverlay);
+  };
+
+  var showPreview = function (picture) {
+    window.picture.setElementPictureData(picture, galleryOverlay, '.gallery-overlay-image', '.likes-count', '.comments-count');
+    document.addEventListener('keydown', onGalleryOverlayEscPress);
+    galleryOverlayClose.addEventListener('keydown', onGalleryOverlayCloseEnterPress);
+    galleryOverlayClose.addEventListener('click', hideGalleryOverlay);
+    galleryOverlay.classList.remove('hidden');
+  };
+
+  var hideGalleryOverlay = function () {
+    document.removeEventListener('keydown', onGalleryOverlayEscPress);
+    galleryOverlayClose.removeEventListener('keydown', onGalleryOverlayCloseEnterPress);
+    galleryOverlayClose.removeEventListener('click', hideGalleryOverlay);
+    galleryOverlay.classList.add('hidden');
+  };
+
+  return {
+
+    showPreviewByUrl: function (pictureUrl) {
+      var pictureData = window.picturesData.find(function (picture) {
+        return picture.url === pictureUrl;
+      });
+      showPreview(pictureData);
+    }
+
+  };
+
+})();
