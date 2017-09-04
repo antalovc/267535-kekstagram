276e499c40105ce6d97fcc43a700d86f09920e40
antalovc <antal.ovchinnikov@gmail.com>
2017-09-04 10:29:13 +0300

    6.20 Исправлена работа галереи

--- js/gallery.js
+++ js/gallery.js
@@ -4,18 +4,20 @@
 
   var OVERLAY_IMAGE_SELECTOR = 'img';
 
+  var picturesData = null;
+
   var addPictureEvents = function () {
     var pictureElements = document.querySelectorAll('.picture');
 
     for (var k = 0; k < pictureElements.length; k++) {
       pictureElements[k].addEventListener('click', function (evt) {
-        window.preview.showPreviewByUrl(evt.currentTarget.querySelector(OVERLAY_IMAGE_SELECTOR).getAttribute('src'));
+        window.preview.showPreviewByUrl(picturesData, evt.currentTarget.querySelector(OVERLAY_IMAGE_SELECTOR).getAttribute('src'));
         evt.preventDefault();
       });
 
       pictureElements[k].addEventListener('keydown', function (evt) {
         window.util.callIfEnterEvent(evt, function () {
-          window.preview.showPreviewByUrl(evt.currentTarget.querySelector(OVERLAY_IMAGE_SELECTOR).getAttribute('src'));
+          window.preview.showPreviewByUrl(picturesData, evt.currentTarget.querySelector(OVERLAY_IMAGE_SELECTOR).getAttribute('src'));
           evt.preventDefault();
         });
       });
@@ -23,7 +25,7 @@
   };
 
   var drawPictures = function (pictures) {
-    window.picturesData = pictures;
+    picturesData = pictures;
 
     var picturesBlock = document.querySelector('.pictures');
     var pictureTemplate = document.querySelector('#picture-template').content;
